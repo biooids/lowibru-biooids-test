9a198983f4ee80ad5495832e9947836000bac56a
biooids <biooidsbiooid@gmail.com>
2024-06-14 23:16:29 +0200

    "Added userRoutes import and mounted it to a test /api/user endpoint"

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -21,3 +22,5 @@ mongoose
   .catch((err) => {
     console.log(`server not working because of ${err}`);
   });
+
+app.use("/api/user", userRoutes);
