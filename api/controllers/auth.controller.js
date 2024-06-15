adc8b28e0ada600403ea478c036247c8dfceb080
biooids <biooidsbiooid@gmail.com>
2024-06-15 21:41:48 +0200

    "Added signin functionality to auth controller and route, installed jsonwebtoken package"

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -28,5 +29,40 @@ export const signup = async (req, res, next) => {
   } catch (err) {
     // res.status(500).json({ message: `signup failed because of ${error}` });
     next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === " ") {
+    next(errorHandler(400, " all fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "User not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password")); // you should not put invalid password because hacker clue
+    }
+
+    // outhenticating the user, we use json web token
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+    // we don't wanna send password or hashed password
+    const { password: pass, ...rest } = validUser._doc; //separating the password and the rest of the fields
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
+  } catch (error) {
+    next(error); // middle ware
   }
 };
