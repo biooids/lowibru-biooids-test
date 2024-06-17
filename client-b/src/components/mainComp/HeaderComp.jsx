b7baf1aa0bc5fb58d9aedf6fb0b3ca19fb7ce1e9
biooids <biooidsbiooid@gmail.com>
2024-06-17 03:02:59 +0200

    "Refactored HeaderComp component: updated imports, added new components, and rearranged layout and styling."

--- client-b/src/components/mainComp/HeaderComp.jsx
+++ client-b/src/components/mainComp/HeaderComp.jsx
@@ -1,73 +1,114 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  DropdownDivider,
+  DropdownItem,
+  Navbar,
+  TextInput,
+} from "flowbite-react";
+import { FaMoon, FaSun } from "react-icons/fa";
+
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "../../app/theme/themeSlice.js";
 
-import { ImSearch } from "react-icons/im";
-import { Dropdown } from "flowbite-react";
-import { IoSettings } from "react-icons/io5";
-import { MdOutlineEvent, MdMenuBook } from "react-icons/md";
-import { FaStoreAlt } from "react-icons/fa";
-import logo from "../../assets/ffwpu.png";
-import { Link } from "react-router-dom";
+function Header() {
+  const path = useLocation().pathname;
 
-function HeaderComp() {
+  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const { theme } = useSelector((state) => state.theme);
   return (
-    <Navbar className="bg-transparent dark:bg-transparent border-b-2  relative flex-wrap">
-      <Link to="/">
-        <Navbar.Brand as={"div"}>
-          <img src={logo} className="mr-1 h-6 sm:h-9" alt="ffwpu Logo" />
-          <span className="self-center whitespace-nowrap text-xl font-semibold">
-            FFWPU
-          </span>
-        </Navbar.Brand>
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="sm:text-xl text-sm self-center whitespace-nowrap font-semibold dark:text-white"
+      >
+        <span className="px-2 py-2 bg-gradient-to-r from-purple-800 via-slate-700 to-cyan-700 text-white rounded-md">
+          gna-biooid
+        </span>
+        blog
       </Link>
-      <form className=" gap-1 flex">
-        <TextInput type="text" placeholder="Search" />
-        <Button
-          type="submit"
-          className="flex justify-center items-center   focus:ring-0"
-        >
-          <ImSearch />
-        </Button>
+
+      <form>
+        <TextInput
+          type="text"
+          placeholder="search"
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
       </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
 
-      <div className="flex">
-        <Link to="events">
-          <Button
-            color="gray "
-            className="hover:bg-amber-900 rounded-l rounded-r-none focus:ring-0"
+      <div className="flex  gap-2 md:order-2">
+        <Button
+          className="w-12 h-10  sm:inline"
+          color="gray"
+          pill
+          onClick={() => {
+            dispatch(toggleTheme());
+          }}
+        >
+          {theme === "light" ? <FaSun /> : <FaMoon />}
+        </Button>
+
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user avatar" img={currentUser.profilePicture} />
+            }
           >
-            <MdOutlineEvent className="mr-3 h-4 w-4" />
-            Events
-          </Button>
-        </Link>
-        <Link to="lectures">
-          <Button
-            color="gray "
-            className="hover:bg-amber-900 rounded-none focus:ring-0"
-          >
-            <MdMenuBook className="mr-3 h-4 w-4" />
-            Ectures
-          </Button>
-        </Link>
-        <Link as="div">
-          <Button
-            color="gray "
-            className="hover:bg-amber-900 rounded-l-none rounded-r focus:ring-0"
-          >
-            <FaStoreAlt className="mr-3 h-4 w-4" />
-            Store
-          </Button>
-        </Link>
+            <Dropdown.Header>
+              <div className="block text-sm">@{currentUser.username}</div>
+              <div className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </div>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <DropdownItem>Profile</DropdownItem>
+              <Dropdown.Item>Sign Out</Dropdown.Item>
+            </Link>
+            <DropdownDivider />
+          </Dropdown>
+        ) : (
+          <Link to="/signin">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In{" "}
+            </Button>
+          </Link>
+        )}
+
+        <Navbar.Toggle />
       </div>
 
-      <div className="flex gap-3 justify-center items-center  ">
-        <Dropdown label={<IoSettings />} className="" color="none">
-          <Dropdown.Item>Light </Dropdown.Item>
-          <Dropdown.Item>Dark</Dropdown.Item>
-        </Dropdown>
-      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/" className="flex justify-center">
+            Home
+          </Link>
+        </Navbar.Link>
+
+        <Navbar.Link active={path === "/lectures"} as={"div"}>
+          <Link to="/lectures" className="flex justify-center">
+            Lectures
+          </Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/events"} as={"div"}>
+          <Link to="/events" className="flex justify-center">
+            events
+          </Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
 
-export default HeaderComp;
+export default Header;
