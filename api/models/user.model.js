eed83781be09c14f19bab5f8a3fc26e7c63d232e
biooids <biooidsbiooid@gmail.com>
2024-06-17 02:19:42 +0200

    "Added Google OAuth authentication feature to API and client-side code"

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,16 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR29Z2Pxk_o5wN43ZJ-ieda1FlP0x01uE-uoZ6jUcb-hZgq99orRaSVSQikwA&s", //add this
+    },
   },
   { timestamps: true } //time stamp for mongo create extra information like the time of creation  and time of update
 );
--- client-b/src/components/OAuth.jsx
+++ client-b/src/components/OAuth.jsx
@@ -1,27 +1,49 @@
 import { Button } from "flowbite-react";
 import React from "react";
+import { FaGoogle } from "react-icons/fa";
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../app/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 function OAuth() {
   const auth = getAuth(app);
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     const provider = new GoogleAuthProvider();
     provider.setCustomParameters({ prompt: "select_account" });
     try {
       const resultsFromGoogle = await signInWithPopup(auth, provider);
-      console.log(resultsFromGoogle);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: { "content-Type": "application/json" },
+        body: JSON.stringify({
+          name: resultsFromGoogle.user.displayName,
+          email: resultsFromGoogle.user.email,
+          googlePhotoUrl: resultsFromGoogle.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
     } catch (error) {
       console.log(error);
     }
   };
   return (
-    <div>
-      <Button type="button" className="w-full" onClick={handleGoogleClick}>
-        Google
-      </Button>
-    </div>
+    <Button
+      type="button"
+      gradientDuoTone="pinkToOrange"
+      outline
+      onClick={handleGoogleClick}
+    >
+      <FaGoogle className="w-6 h-6 text-red-700 mr-2" />
+      Continue with google
+    </Button>
   );
 }
 
