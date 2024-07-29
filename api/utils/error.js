132fdeefdb5dbc7b6be2afd82ac7f75d0476b9ef
biooids <biooidsbiooid@gmail.com>
2024-07-29 08:01:46 +0200

    removes back end files

--- api/utils/error.js
+++
@@ -1,6 +0,0 @@
-export const errorHandler = (statusCode, message) => {
-  const error = new Error(); //error constructor from javascript
-  error.statusCode = statusCode;
-  error.message = message;
-  return error;
-};
